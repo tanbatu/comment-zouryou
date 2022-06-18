25a1aded5c72af2d764c97102be19e7db4264192
tanbatu <umewi2005@gmail.com>
2022-06-19 00:34:24 +0900

    Edited comment stroke style

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ function PLAYCOMMENT() {
         console.log(COMMENT);
         niconiComments = new NiconiComments(zouryouCanvasElement, COMMENT, {
             video: videoElement,
-            enableLegacyPiP: true
+            enableLegacyPiP: true,
+            useLegacy: true
         });
         //video.ontimeupdateを使用すると、呼び出し回数の関係でコメントカクつく
         draw = setInterval(() => niconiComments.drawCanvas(Math.floor(videoElement.currentTime * 100)), 10);
@@ -128,9 +129,9 @@ const sleep = (time) => {
     })
 }
 
-
-setTimeout(function() {
-    document.getElementsByClassName('VideoOverlayContainer')[0].insertAdjacentHTML('beforeend', `
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementsByClassName('VideoOverlayContainer')[0].insertAdjacentHTML('beforeend', `
         <div id='allcommentsetting' style='display: none;background-color:rgb(0,0,0,0.8);width:200px;height:100px;padding:2px;position:absolute;right:40px;bottom:0;'>
             <p style='color:white;margin:0'>コメントを倍増</p>
             <small style='font-size:10px;color:whitesmoke'>最大30程度が推奨です。それ以上呼び出すとPCがフリーズする可能性があります。</small>
@@ -138,7 +139,7 @@ setTimeout(function() {
             <button id='zenkomebutton'>読み込み開始！</button>
         </div>
         `);
-    (document.getElementsByClassName('PlayerRepeatOnButton')[0] || document.getElementsByClassName('PlayerRepeatOffButton')[0]).insertAdjacentHTML('beforebegin', `
+        (document.getElementsByClassName('PlayerRepeatOnButton')[0] || document.getElementsByClassName('PlayerRepeatOffButton')[0]).insertAdjacentHTML('beforebegin', `
         <button data-title='全コメ表示' type='button' class='ActionButton ControllerButton' id='AllCommentViewButton' style='margin: 0 -4px;'>
             <div class='ControllerButton-inner' style='transform: scale(100%,87%);'>
                 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 20' width='48px' height='48px'>
@@ -148,48 +149,47 @@ setTimeout(function() {
         </button>
         `);
 
-    let customStyle = document.createElement('style');
-    customStyle.innerHTML = '.CustomVideoContainer{width: 640px;height: 360px;}body.is-large:not(.is-fullscreen) .CustomVideoContainer {width: 854px;height: 480px;}body.is-fullscreen .CustomVideoContainer {width: 100vw !important;height: 100vh !important;}@media screen and (min-width: 1286px) and (min-height: 590px){body.is-autoResize:not(.is-fullscreen) .CustomVideoContainer {width: 854px;height: 480px;}@media screen and (min-width: 1392px) and (min-height: 650px){body.is-autoResize:not(.is-fullscreen) .CustomVideoContainer {width: 960px;height: 540px;}}';
-    document.body.appendChild(customStyle);
-    CommentRenderer = document.getElementsByClassName('CommentRenderer')[0];
-    defaultCanvasElement = CommentRenderer.children[0];
+        let customStyle = document.createElement('style');
+        customStyle.innerHTML = '.CustomVideoContainer{width: 640px;height: 360px;}body.is-large:not(.is-fullscreen) .CustomVideoContainer {width: 854px;height: 480px;}body.is-fullscreen .CustomVideoContainer {width: 100vw !important;height: 100vh !important;}@media screen and (min-width: 1286px) and (min-height: 590px){body.is-autoResize:not(.is-fullscreen) .CustomVideoContainer {width: 854px;height: 480px;}@media screen and (min-width: 1392px) and (min-height: 650px){body.is-autoResize:not(.is-fullscreen) .CustomVideoContainer {width: 960px;height: 540px;}}';
+        document.body.appendChild(customStyle);
+        CommentRenderer = document.getElementsByClassName('CommentRenderer')[0];
+        videoElement = document.getElementById('MainVideoPlayer').children[0];
+        VideoSymbolContainer = document.getElementsByClassName('VideoSymbolContainer')[0];
+        PlayerContainer = document.getElementsByClassName('PlayerContainer')[0];
+        DefaultVideoContainer = document.getElementsByClassName('InView VideoContainer')[0];
+        CustomVideoContainer = document.createElement('div');
+        CustomVideoContainer.innerHTML = '<div class="CommentRenderer"><canvas id="zouryouCanvasElement" width="1920" height="1080"></canvas><video id="pipVideoElement"></video></div>';
+        CustomVideoContainer.classList.add('CustomVideoContainer', 'InView');
+        PlayerContainer.children[0].after(CustomVideoContainer);
+        zouryouCanvasElement = document.getElementById('zouryouCanvasElement');
+        pipVideoElement = document.getElementById('pipVideoElement');
+        CommentLoadingScreenWrapper = document.createElement('div');
+        CommentLoadingScreenWrapper.innerHTML = '<div id="CommentLoadingScreen"></div>';
+        PlayerContainer.appendChild(CommentLoadingScreenWrapper);
+        CommentLoadingScreen = document.getElementById('CommentLoadingScreen');
+        CustomVideoContainer.style.display = 'none';
+        zouryouCanvasElement.style = 'position:absolute;top:0;left:0;width:100%;height:100%;z-index:0;display:none';
+        pipVideoElement.style = 'position:absolute;top:0;left:0;width:100%;height:100%;z-index:1;pointer-events:all';
+        pipVideoElement.onpause = () => {
+            pipVideoElement.play();
+        };
+        zouryouCanvasElement.id = 'zouryou_comment';
 
-    videoElement = document.getElementById('MainVideoPlayer').children[0];
-    VideoSymbolContainer = document.getElementsByClassName('VideoSymbolContainer')[0];
-    PlayerContainer = document.getElementsByClassName('PlayerContainer')[0];
-    DefaultVideoContainer = document.getElementsByClassName('InView VideoContainer')[0];
-    CustomVideoContainer = document.createElement('div');
-    CustomVideoContainer.innerHTML = '<div class="CommentRenderer"><canvas id="zouryouCanvasElement" width="1920" height="1080"></canvas><video id="pipVideoElement"></video></div>';
-    CustomVideoContainer.classList.add('CustomVideoContainer', 'InView');
-    PlayerContainer.children[0].after(CustomVideoContainer);
-    zouryouCanvasElement = document.getElementById('zouryouCanvasElement');
-    pipVideoElement = document.getElementById('pipVideoElement');
-    CommentLoadingScreenWrapper = document.createElement('div');
-    CommentLoadingScreenWrapper.innerHTML = '<div id="CommentLoadingScreen"></div>';
-    PlayerContainer.appendChild(CommentLoadingScreenWrapper);
-    CommentLoadingScreen = document.getElementById('CommentLoadingScreen');
-    CustomVideoContainer.style.display = 'none';
-    zouryouCanvasElement.style = 'position:absolute;top:0;left:0;width:100%;height:100%;z-index:0;display:none';
-    pipVideoElement.style = 'position:absolute;top:0;left:0;width:100%;height:100%;z-index:1;pointer-events:all';
-    pipVideoElement.onpause = () => {
-        pipVideoElement.play();
-    };
-    zouryouCanvasElement.id = 'zouryou_comment';
-
-    const setting = document.getElementById('allcommentsetting');
-    document.getElementById('AllCommentViewButton').addEventListener('click', () => {
-        if (setting.style.display === 'none') {
-            setting.style.display = 'block';
-        } else {
+        const setting = document.getElementById('allcommentsetting');
+        document.getElementById('AllCommentViewButton').addEventListener('click', () => {
+            if (setting.style.display === 'none') {
+                setting.style.display = 'block';
+            } else {
+                setting.style.display = 'none';
+            }
+        }, false);
+        document.getElementById('zenkomebutton').onclick = () => {
+            let num = document.getElementById('load_num').value;
             setting.style.display = 'none';
-        }
-    }, false);
-    document.getElementById('zenkomebutton').onclick = () => {
-        let num = document.getElementById('load_num').value;
-        setting.style.display = 'none';
-        CommentLimit = (num !== '') ? Number(num) : 20;
-        CommentLoadingScreenWrapper.style = 'width: 100%;position: absolute;height: 100%;background-color: #999;z-index: 6;opacity: 0.8;font-size:20px;color:black;overflow: scroll;top:0;left:0';
-        document.getElementById('zenkomebutton').disabled = true;
-        setTimeout(LOADCOMMENT, 2000);
-    };
-}, 1000);
+            CommentLimit = (num !== '') ? Number(num) : 20;
+            CommentLoadingScreenWrapper.style = 'width: 100%;position: absolute;height: 100%;background-color: #999;z-index: 6;opacity: 0.8;font-size:20px;color:black;overflow: scroll;top:0;left:0';
+            document.getElementById('zenkomebutton').disabled = true;
+            setTimeout(LOADCOMMENT, 2000);
+        };
+    }, 1000);
+};
